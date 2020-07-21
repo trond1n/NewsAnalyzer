b876fcaaf04cfec3a384d3a6f84d50879d924b7f
Yuri Trondin <manctzns@gmail.com>
2020-07-21 11:21:17 +0500

    дополнил сборку новыми плагинами

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require('webpack');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
     entry: './src/script/script.js',
@@ -16,8 +20,12 @@ module.exports = {
             exclude: /node_modules/
         },
         {
-            test: /\.css$/,
-            use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+            test: /\.css$/i,
+            use: [
+                (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+                'css-loader',
+                'postcss-loader'
+            ]
         },
         {
             test: /\.(eot|ttf|woff|woff2)$/,
@@ -43,9 +51,23 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html'
+            template: 'src/index.html'
         }),
-        new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
+        new webpack.DefinePlugin({
+            'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        }),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorPluginOptions: {
+                preset: ['default'],
+            },
+            canPrint: true
+        }),
         new WebpackMd5Hash()
+
     ]
 };
